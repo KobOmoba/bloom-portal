2afa76f203569c83f135af247caea0c89f4e23f6
KobOmoba <aadesanya173@gmail.com>
2026-05-26 13:20:09 +0100

    Add service worker

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE='edubloom-v1';
+const ASSETS=['.','./index.html'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const rc=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,rc)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
